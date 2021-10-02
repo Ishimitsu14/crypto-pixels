a6be7efbeefd4e5ebef9de6b624af0bf2dc38aef
Kirill Kontorovich <ishimitsu.kk@gmail.com>
2021-10-02 15:37:39 +0300

    add command specs for discord bot

--- bots/discord/app.ts
+++ bots/discord/app.ts
@@ -4,60 +4,122 @@ import {Product} from "../../models/Product";
 import {connect} from "../../functions";
 import RarityService from "../../services/RarityService";
 
+const undefinedUnicornCommand = async (response: Discord.Message) => {
+    await response.reply('This Unicorn not minted yet. Check your Unicorn id!')
+}
 
-const getEmbed = (product: Product, rarityService: RarityService) => {
-    const attributes: { name: string; value: string; inline: boolean }[] = []
-    const rarities = rarityService.getRaritiesByProduct(product)
-    let index = 0
-    rarities.forEach(({ trait_type, value, rarity }) => {
-        let rarityPrefix = ''
-        switch (rarity) {
-            case 'Common':
-                rarityPrefix = ''
-                break
-            case 'Uncommon':
-                rarityPrefix = 'yml'
-                break
-            default:
-                rarityPrefix = 'arm'
-                break
+const errorCommand = async (response: Discord.Message) => {
+    await response.reply('I do not know such a command. Check "bot-usage" channel for info. 🤓')
+}
+
+const specsCommand = async (message: string, response: Discord.Message) => {
+    if (message && !Number.isNaN(parseInt(message))) {
+        const product = await Product.findOne(
+            {
+                where: {
+                    id: parseInt(message),
+                    status: Product.statuses.SOLD
+                }
+            }
+        )
+        if (product) {
+            const embed = {
+                color: '#ff0000',
+                title: product.metaData.name,
+                url: `https://opensea.io/assets/0x1a8fe1959e288c51295f7d44e3991799685eac83/${product.id}`,
+                author: {
+                    name: 'Specs of Unicorns',
+                    icon_url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
+                    url: 'https://ucmv.io',
+                },
+                thumbnail: {
+                    url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
+                },
+                fields: product.stats,
+                image: {
+                    url: `https://ipfs.io/ipfs/${process.env.PINATA_PRODUCT_CID}/${product.uuid}/1.png`,
+                },
+                timestamp: new Date(),
+            }
+            await response.channel.send({ embed })
+        } else {
+            await undefinedUnicornCommand(response)
         }
-        const rarityWithColor = "```" + rarityPrefix + '\n' +
-            rarity +
-            '```'
-        attributes.push({
-            name: trait_type,
-            value: `${value}:
+    } else {
+        await undefinedUnicornCommand(response)
+    }
+}
+
+const checkCommand = async (message: string, response: Discord.Message, rarityService: RarityService) => {
+    if (message && !Number.isNaN(parseInt(message))) {
+        const product = await Product.findOne(
+            {
+                where: {
+                    id: parseInt(message),
+                    status: Product.statuses.SOLD
+                }
+            }
+        )
+        if (product) {
+            const attributes: { name: string; value: string; inline: boolean }[] = []
+            const rarities = rarityService.getRaritiesByProduct(product)
+            let index = 0
+            rarities.forEach(({ trait_type, value, rarity }) => {
+                let rarityPrefix = ''
+                switch (rarity) {
+                    case 'Common':
+                        rarityPrefix = ''
+                        break
+                    case 'Uncommon':
+                        rarityPrefix = 'yml'
+                        break
+                    default:
+                        rarityPrefix = 'arm'
+                        break
+                }
+                const rarityWithColor = "```" + rarityPrefix + '\n' +
+                    rarity +
+                    '```'
+                attributes.push({
+                    name: trait_type,
+                    value: `${value}:
                                 ${rarityWithColor}`,
-            inline: true
-        })
-        index += 1
-        if (index === 2) {
-            attributes.push({
-                name: '\u200b',
-                value: '\u200b',
-                inline: false,
+                    inline: true
+                })
+                index += 1
+                if (index === 2) {
+                    attributes.push({
+                        name: '\u200b',
+                        value: '\u200b',
+                        inline: false,
+                    })
+                    index = 0
+                }
             })
-            index = 0
+            const embed = {
+                color: '#ff0000',
+                title: product.metaData.name,
+                url: `https://opensea.io/assets/0x1a8fe1959e288c51295f7d44e3991799685eac83/${product.id}`,
+                author: {
+                    name: 'Rarity of Unicorns',
+                    icon_url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
+                    url: 'https://ucmv.io',
+                },
+                thumbnail: {
+                    url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
+                },
+                fields: attributes,
+                image: {
+                    url: `https://ipfs.io/ipfs/${process.env.PINATA_PRODUCT_CID}/${product.uuid}/1.png`,
+                },
+                timestamp: new Date(),
+            }
+            await response.channel.send({ embed })
+        } else {
+            await undefinedUnicornCommand(response)
         }
-    })
-    return  {
-        color: '#ff0000',
-        title: product.metaData.name,
-        url: `https://opensea.io/assets/0x1a8fe1959e288c51295f7d44e3991799685eac83/${product.id}`,
-        author: {
-            name: 'Rarity of Unicorns',
-            icon_url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
-            url: 'https://ucmv.io',
-        },
-        thumbnail: {
-            url: 'https://i.ibb.co/LZ74FyZ/favicon.png',
-        },
-        fields: attributes,
-        image: {
-            url: `https://ipfs.io/ipfs/${process.env.PINATA_PRODUCT_CID}/${product.uuid}/1.png`,
-        },
-        timestamp: new Date(),
+    } else {
+        await undefinedUnicornCommand(response)
     }
 }
 
@@ -73,34 +135,29 @@ const getEmbed = (product: Product, rarityService: RarityService) => {
         if (response.author.bot) return;
         if (!response.content.startsWith(prefix)) return;
 
-        let message = ''
         const commandBody = response.content.slice(prefix.length)
         const args = commandBody.split(' ')
         const command = args.shift()
+        const message = args[0] ? args[0] : ''
+
         if (command) {
             switch (command.toLowerCase()) {
                 case 'check':
-                    message = args[0] ? args[0] : ''
+                    try {
+                        await checkCommand(message, response, rarityService)
+                    } catch (e: any) {
+                        await errorCommand(response)
+                    }
+                    break
+                case 'specs':
+                    try {
+                        await specsCommand(message, response)
+                    } catch (e: any) {
+                        await errorCommand(response)
+                    }
                     break
                 default:
-                    message = ''
-            }
-            if (message && !Number.isNaN(parseInt(message))) {
-                const product = await Product.findOne(
-                    {
-                        where: {
-                            id: parseInt(message),
-                            status: Product.statuses.SOLD
-                        }
-                    }
-                )
-                if (product) {
-                    await response.channel.send({ embed: getEmbed(product, rarityService) })
-                } else {
-                    await response.reply('This Unicorn not minted yet. Check your Unicorn id!')
-                }
-            } else {
-                await response.reply('This Unicorn not minted yet. Check your Unicorn id!')
+                    await errorCommand(response)
             }
         }
     })
